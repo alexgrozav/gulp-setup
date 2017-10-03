70ff2d2cb0c47ddb849fb022957a646f224a0600
Alex Grozav <alex.grozav@visma.com>
2017-10-03 16:41:55 +0300

    Initial configuration layout.

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,7 @@
+const gulp = require('gulp');
+const plugins = require('gulp-load-plugins')();
 const path = require('path');
+
 
 module.exports = (config) => {
   //
@@ -11,46 +14,55 @@ module.exports = (config) => {
     tasks: {
       html: {
         task: path.join(__dirname, 'tasks', 'html'),
-        pattern: path.join('**', '*.html')
-      },
-      pug: {
-        task: path.join(__dirname, 'tasks', 'pug'),
-        pattern: path.join('**', '*.pug')
-      },
-      javascript: {
-        task: path.join(__dirname, 'tasks', 'javascript'),
-        pattern: path.join('**', '*.js')
-      },
-      coffeescript: {
-        task: path.join(__dirname, 'tasks', 'coffeescript'),
-        pattern: path.join('**', '*.coffee')
-      },
-      typescript: {
-        task: path.join(__dirname, 'tasks', 'typescript'),
-        pattern: path.join('**', '*.ts')
-      },
-      css: {
-        task: path.join(__dirname, 'tasks', 'css'),
-        pattern: path.join('**', '*.css')
-      },
-      stylus: {
-        task: path.join(__dirname, 'tasks', 'stylus'),
-        pattern: path.join('**', '*.styl')
-      },
-      sass: {
-        task: path.join(__dirname, 'tasks', 'sass'),
-        pattern: path.join('**', '*.{sass,scss}')
-      },
-      image: {
-        task: path.join(__dirname, 'tasks', 'image'),
-        pattern: path.join('**', '*.{svg,jpg,png,gif}')
-      },
-      font: {
-        task: path.join(__dirname, 'tasks', 'font'),
-        pattern: path.join('**', '*.{eot,svg,ttf,woff,woff2}')
+        base: path.join(__dirname, 'base', 'compile'),
+        pattern: path.join('**', '*.html'),
+        cache: true
+      // },
+      // pug: {
+      //   task: path.join(__dirname, 'tasks', 'pug'),
+      //   pattern: path.join('**', '*.pug')
+      // },
+      // javascript: {
+      //   task: path.join(__dirname, 'tasks', 'javascript'),
+      //   pattern: path.join('**', '*.js')
+      // },
+      // coffeescript: {
+      //   task: path.join(__dirname, 'tasks', 'coffeescript'),
+      //   pattern: path.join('**', '*.coffee')
+      // },
+      // typescript: {
+      //   task: path.join(__dirname, 'tasks', 'typescript'),
+      //   pattern: path.join('**', '*.ts')
+      // },
+      // css: {
+      //   task: path.join(__dirname, 'tasks', 'css'),
+      //   pattern: path.join('**', '*.css')
+      // },
+      // stylus: {
+      //   task: path.join(__dirname, 'tasks', 'stylus'),
+      //   pattern: path.join('**', '*.styl')
+      // },
+      // sass: {
+      //   task: path.join(__dirname, 'tasks', 'sass'),
+      //   pattern: path.join('**', '*.{sass,scss}')
+      // },
+      // image: {
+      //   task: path.join(__dirname, 'tasks', 'image'),
+      //   pattern: path.join('**', '*.{svg,jpg,png,gif}')
+      // },
+      // font: {
+      //   task: path.join(__dirname, 'tasks', 'font'),
+      //   pattern: path.join('**', '*.{eot,svg,ttf,woff,woff2}')
       }
     }
   }, config);
 
 
+  Object.keys(config.tasks).forEach((name) => {
+    if (config.tasks[name].base) {
+      gulp.task(name, require(config.tasks[name].base)(gulp, plugins, config));
+    } else {
+      gulp.task(name, require(config.tasks[name].task)(gulp, plugins, config));
+    }
+  });
 }
