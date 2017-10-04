c8c2a3e19c43973b96703c86c8ac13e2e3d9c2e6
Alex Grozav <alex.grozav@visma.com>
2017-10-04 14:29:21 +0300

    Added base task for compilation.

--- base/compile.js
+++ base/compile.js
@@ -1,3 +1,27 @@
-module.exports = () => {
+module.exports = (gulp, plugins, config, task) => () => {
+  return gulp.src(plugins['path'].join(config.src, task.pattern))
+    // Cache files that have been already processed
+    //
+    .pipe(plugins['if'](config.cache, plugins['cached'](task.name)))
 
+    // List files that are being processed
+    //
+    .pipe(plugins['if'](config.debug, plugins['debug']()))
+
+    // Use plumber to catch unhandled exceptions
+    //
+    .pipe(plugins['plumber']())
+
+    // Process files for local development use
+    //
+    .pipe(plugins['if'](config.build, () => plugins['lazypipe']()
+      .pipe(gulp.dest(config.build))
+      .pipe(plugins['browser-sync'].stream())
+    ))
+
+    // Process files for distribution
+    //
+    .pipe(plugins['if'](config.build, () => plugins['lazypipe']()
+      .pipe(gulp.dest(config.dist))
+    ))
 }
