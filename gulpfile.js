c2abfeb5e8b6173e792979f18eb04b121ca64392
Alex Grozav <alex@grozav.com>
2017-10-05 23:43:16 +0300

    Added sass and css task.

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,7 @@ const plugins = require('gulp-load-plugins')();
 const path = require('path');
 
 
-require(path.join(__dirname, 'index'))(gulp, plugins, {
+require(path.join(__dirname, 'index'))(plugins, gulp, {
   src: path.join(__dirname, 'tests', 'src'),
   build: path.join(__dirname, 'tests', 'build'),
   dist: path.join(__dirname, 'tests', 'dist'),
