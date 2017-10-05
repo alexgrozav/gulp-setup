a61878d371eb5697aad0fd0e166df6bf901590cb
Alex Grozav <alex@grozav.com>
2017-10-05 23:43:50 +0300

    Updated task loader to automatically initialize steps with noop.

--- helpers/task-loader.js
+++ helpers/task-loader.js
@@ -1,9 +1,14 @@
-module.exports = (gulp, plugins, config, task) => {
-  if (typeof task === 'function') {
-    return task.task();
-  } else if (typeof task === 'object') {
-    return task.task;
-  } else {
-    return require(task.base || task.task)(gulp, plugins, config, task);
-  }
+let isString = (v) => typeof v === 'string' || v instanceof String
+
+module.exports = ($, gulp, config, task) => {
+  if (isString(task.base)) task.base = require(task.base)($, gulp, config, task);
+  if (isString(task.process)) {
+    task.process = require(task.process)($, gulp, config, task);
+
+    ['init', 'build', 'dist', 'end'].forEach((step) => {
+      task.process[step] = task.process[step] || $.util.noop;
+    });
+  };
+
+  return task.base || task.process;
 }
