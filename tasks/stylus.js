9181a9d8d3cae357a1abcbe92abbd764f9458530
Alex Grozav <alex.grozav@visma.com>
2017-10-06 15:39:42 +0300

    Added stylus task.

---
+++ tasks/stylus.js
@@ -0,0 +1,51 @@
+module.exports = ($, gulp, config, task) => {
+  const poststylus = require('poststylus');
+  const partial = require($.path.join(__dirname, '..', 'helpers', 'partial'))($, task, {
+    ext: ['styl']
+  });
+
+
+  /**
+   * Configuration for replacing Webpack Stylus node_modules import syntax
+   */
+  let normalizeImport = {
+    pattern: /\@import ([\'\"])\~/g,
+    replace: (replacement, capture)  => '@import ' + capture,
+    options: {
+      logs: { enabled: false }
+    }
+  };
+
+
+  /**
+   * Stylus compilation options
+   */
+  let options = {
+    'resolve url': true,
+    'include css': true,
+    'include': 'node_modules',
+    'modulesDirectories': ['node_modules'],
+    'url': 'embedurl',
+    'use': [
+      poststylus([
+        'autoprefixer',
+        'rucksack-css'
+      ])
+    ]
+  };
+
+
+  return {
+    init: $.lazypipe()
+      .pipe($.tap, partial.process),
+
+    build: $.lazypipe()
+      .pipe($.ignore, partial.check)
+      .pipe($.ignore, /index\.styl$/)
+      .pipe($.stringReplace, normalizeImport.pattern, normalizeImport.replace, normalizeImport.options)
+      .pipe($.stylus, options),
+
+    dist: $.lazypipe()
+      .pipe($.cssmin)
+  }
+};
