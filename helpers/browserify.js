014ded724a959311ec97b836e3a53b7e6cf92fb1
Alex Grozav <alex@grozav.com>
2017-10-09 21:09:24 +0300

    Added support for dynamically applying transforms.

--- helpers/browserify.js
+++ helpers/browserify.js
@@ -1,11 +1,20 @@
 const browserify = require('browserify');
 
 
-module.exports = ($, task, options) => $.lazypipe()
-  .pipe($.through.obj, (file, enc, next) => {
-    browserify(file.path, task.options.browserify).bundle((err, res) => {
-      file.contents = res;
-      next(err, file);
-    });
-  })
-  .pipe($.buffer);
+module.exports = ($, task, options) => {
+  let transforms = task.options.browserify;
+
+  return $.lazypipe()
+    .pipe($.through.obj, (file, enc, next) => {
+      let bundle = browserify(file.path, options);
+      options.transforms.forEach((transform) => {
+        bundle.transform(transforms[transform].transform, transforms[transform].options || {});
+      });
+
+      bundle.bundle((err, res) => {
+        file.contents = res;
+        next(err, file);
+      });
+    })
+    .pipe($.buffer);
+}
