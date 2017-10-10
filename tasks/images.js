c635d6c2dc92cb1b2e2d18693d7de66ce8eee824
Alex Grozav <alex@grozav.com>
2017-10-10 21:13:16 +0300

    Ignoring svg files containing the word font.

--- tasks/images.js
+++ tasks/images.js
@@ -1,6 +1,6 @@
 
 module.exports = ($, gulp, config, task) => ({
   build: $.lazypipe()
-    .pipe($.ignore, (file) => /(font)/.test(file.relative))
+    .pipe($.ignore, (file) => /font.*\.svg$/.test(file.relative))
     .pipe($.imagemin, { verbose: true })
 });
