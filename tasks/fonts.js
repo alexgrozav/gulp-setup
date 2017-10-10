7d15e26dba63fc38c566cf14c1d636b0f0f78b45
Alex Grozav <alex@grozav.com>
2017-10-10 21:12:18 +0300

    Removed ignore statement from fonts.

--- tasks/fonts.js
+++ tasks/fonts.js
@@ -1,4 +1,1 @@
-module.exports = ($, gulp, config, task) => ({
-  init: $.lazypipe()
-    .pipe($.ignore, (file) => /(img)/.test(file.relative)),
-});
+module.exports = ($, gulp, config, task) => ({});
