8e5164b9589bd26dca1dcd75719f0c4ec6c91f30
Alex Grozav <alex@grozav.com>
2017-10-11 22:18:00 +0300

    Updated tasks loader to allow task extensions.

--- helpers/task-loader.js
+++ helpers/task-loader.js
@@ -2,7 +2,15 @@ let isString = (v) => typeof v === 'string' || v instanceof String
 
 
 module.exports = ($, gulp, config, task) => {
-  if (isString(task.base)) task.base = require(task.base)($, gulp, config, task);
+  if (task.extends) {
+    task.base = config.tasks[task.extends].base;
+    task.process = config.tasks[task.extends].process;
+  }
+
+  if (isString(task.base)) {
+    task.base = require(task.base)($, gulp, config, task);
+  }
+
   if (isString(task.process)) {
     task.process = require(task.process)($, gulp, config, task);
 
