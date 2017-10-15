68fb07250bdd5bd7b08fc067cf265b9ef8ffd929
Alex Grozav <alex@grozav.com>
2017-10-15 22:22:57 +0300

    Added support for adding a task to build using related params

--- tasks/build.js
+++ tasks/build.js
@@ -2,7 +2,9 @@ module.exports = ($, gulp, config, task) => () => {
   let tasks = [];
 
   Object.keys(config.tasks).forEach((name) => {
-    if (config.tasks[name].pattern) tasks.push(name);
+    if (config.tasks[name].pattern || config.tasks[name].related.build) {
+      tasks.push(name);
+    }
   });
 
   return $.runSequence('clean', tasks);
