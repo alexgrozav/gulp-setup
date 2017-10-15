183260f0667604f46c0ea03eefbccf592c9203fe
Alex Grozav <alex@grozav.com>
2017-10-15 20:09:46 +0300

    Improved gulp clean array check.

--- tasks/clean.js
+++ tasks/clean.js
@@ -3,7 +3,7 @@ module.exports = ($, gulp, config, task) => {
   if (!!config.paths.build) paths.push(config.paths.build);
   if (!!config.paths.dist) paths.push(config.paths.dist);
   if (!!task.pattern) {
-    if (Array.isArray(task.pattern)) {
+    if (task.pattern.constructor === Array) {
       paths = paths.concat(task.pattern);
     } else {
       paths.push(task.pattern);
